3a4590369dce38fa87390d6b6e2185b16dc5519f
Ivan Nikolaev <norf33rus@gmail.com>
2016-12-11 21:40:03 +0300

    nothing

--- e2e/dashboard/dashboard.e2e-spec.ts
+++ e2e/dashboard/dashboard.e2e-spec.ts
@@ -3,39 +3,41 @@ import { browser, by } from 'protractor';
 
 describe('Dashboard E2E Tests', function () {
 
-let sessionId: any;
+    let sessionId:any;
 
     browser.getSession().then((session) => {
         sessionId = session.getId();
     });
-    
-    afterAll((done) => {
-        browser.params.souceLab.updateJob(sessionId, {
-            build: browser.params.build
-        }, done);
+
+    if (browser.params.souceLab) {
+        afterAll((done) => {
+            browser.params.souceLab.updateJob(sessionId, {
+                build: browser.params.build
+            }, done);
+        });
+    }
+
+    beforeAll(function () {
+        browser.ignoreSynchronization = false;
+        browser.get('/');
     });
-    
- beforeAll(function () {
-   browser.ignoreSynchronization = false;
-   browser.get('/');
-  });
 
- it('Should be displayed 4 heroes', function () {
-    expect(DashboardPage.hero.count()).toEqual(4);
-  });
+    it('Should be displayed 4 heroes', function () {
+        expect(DashboardPage.hero.count()).toEqual(4);
+    });
 
-  it('SearchComponent should be displayed', function () {
-    expect(DashboardPage.searchComponent.isDisplayed()).toBe(true);
-  });
+    it('SearchComponent should be displayed', function () {
+        expect(DashboardPage.searchComponent.isDisplayed()).toBe(true);
+    });
 
-  it('Searched hero should be found', function () {
-    DashboardPage.searchBox.sendKeys('Bombasto');
-    expect(DashboardPage.searchResult.count()).toEqual(1);
-  });
+    it('Searched hero should be found', function () {
+        DashboardPage.searchBox.sendKeys('Bombasto');
+        expect(DashboardPage.searchResult.count()).toEqual(1);
+    });
 
-  it('It should go to edit', function () {
-    DashboardPage.searchResult.first().click();
-    expect(browser.getCurrentUrl()).toContain('detail');
-  });
-  
+    it('It should go to edit', function () {
+        DashboardPage.searchResult.first().click();
+        expect(browser.getCurrentUrl()).toContain('detail');
+    });
+
 });
--- e2e/list/list.e2e-spec.ts
+++ e2e/list/list.e2e-spec.ts
@@ -2,31 +2,33 @@ import ListPage from './list.page';
 import { browser, by } from 'protractor';
 
 describe('List E2E Tests', function () {
-  let sessionId: any;
+    let sessionId:any;
 
     browser.getSession().then((session) => {
         sessionId = session.getId();
     });
-    
-    afterAll((done) => {
-        browser.params.souceLab.updateJob(sessionId, {
-            build: browser.params.build
-        }, done);
+
+    if (browser.params.souceLab) {
+        afterAll((done) => {
+            browser.params.souceLab.updateJob(sessionId, {
+                build: browser.params.build
+            }, done);
+        });
+    }
+
+    beforeAll(function () {
+        browser.ignoreSynchronization = false;
+        browser.get('/');
+        ListPage.linkHeroes.click();
     });
 
- beforeAll(function () {
-   browser.ignoreSynchronization = false;
-   browser.get('/');
-   ListPage.linkHeroes.click();
-  });
+    it('heroesContent should not be displayed', function () {
+        expect(ListPage.heroesContent.isDisplayed()).toBe(true);
+    });
 
- it('heroesContent should not be displayed', function () {
-    expect(ListPage.heroesContent.isDisplayed()).toBe(true);
-  });
-
-  it('btnDetail should be displayed', function () {
-    ListPage.heroList.all(by.xpath("./li")).get(0).click();
-    expect(ListPage.currentHero.isDisplayed()).toBe(true);
-    expect(ListPage.btnDetail.isDisplayed()).toBe(true);
-  });
+    it('btnDetail should be displayed', function () {
+        ListPage.heroList.all(by.xpath("./li")).get(0).click();
+        expect(ListPage.currentHero.isDisplayed()).toBe(true);
+        expect(ListPage.btnDetail.isDisplayed()).toBe(true);
+    });
 });
